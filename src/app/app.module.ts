bcd673858376d9da35fb01d693dd424de4f88984
ahmedine <krapulxx@gmail.com>
2022-04-19 23:54:53 +0200

    First page
    First page // with routes // architecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {HomeModule} from "./home/home.module";
+import {AppRoutingModule} from "./app-routing.module";
+import {SharedModule} from "./shared/shared.module";
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    HomeModule,
+    SharedModule,
+    //
+    AppRoutingModule
+  ],
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
+  providers: [
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
