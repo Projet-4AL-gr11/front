0b23d4404f6869a4e8fffe412cd78335acd61fba
tbriand <briandthibaud99@gmail.com>
2022-05-22 18:14:43 +0200

    [add] update app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {HomeModule} from "./components/home/home.module";
+import {HomeModule} from "./components/page_/home/home.module";
 import {AppRoutingModule} from "./app-routing.module";
 import {SharedModule} from "./shared/shared.module";
-import {LoginModule} from "./components/login/login.module";
-import {RegisterModule} from "./components/register/register.module";
-import {ProfileModule} from "./components/profile/profile.module";
-import {CodeModule} from "./components/code/code.module";
-import {SocialModule} from "./components/social/social.module";
+import {LoginModule} from "./components/page_/auth_/login/login.module";
+import {RegisterModule} from "./components/page_/auth_/register/register.module";
+import {ProfileModule} from "./components/page_/profile/profile.module";
+import {CodeModule} from "./components/page_/code/code.module";
+import {SocialModule} from "./components/page_/social/social.module";
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ImageUrlPipe } from './pipes/image-url/image-url.pipe';
 
 @NgModule({
   imports: [
@@ -26,6 +28,8 @@ import {SocialModule} from "./components/social/social.module";
   ],
   declarations: [
     AppComponent,
+    NavbarComponent,
+    ImageUrlPipe,
   ],
   providers: [
   ],
