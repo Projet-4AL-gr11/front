9b0f8e318a51c4477af1dd1833a21bfd50a7f43d
tbriand <briandthibaud99@gmail.com>
2022-05-27 14:17:42 +0200

    [add] basic page for connection and subscribe

--- src/app/components/page_/auth_/login/login.component.ts
+++ src/app/components/page_/auth_/login/login.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../../../../services/auth/auth.service";
+import {Router} from "@angular/router";
+import {Title} from "@angular/platform-browser";
+import {environment} from "../../../../../environments/environment";
+import {firstValueFrom} from "rxjs";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +12,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  userForm: FormGroup;
+  submitted: boolean;
+  error: boolean;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder,
+              private _authService: AuthService,
+              private router: Router,
+              private _titleService: Title) {
+    this._titleService.setTitle("Connexion - " + environment.name);
+    this.error = false;
+    this.submitted = false;
   }
 
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  onSubmitForm() {
+    this.submitted = true;
+    const formValue = this.userForm.value;
+    firstValueFrom(this._authService.login(formValue.username, formValue.password))
+      .then().catch(error => {
+      this.submitted = false;
+      if (error.status === 401) {
+        this.error = true;
+      }
+    }).finally(() => {
+      this.router.navigate(['/']).then();
+    });
+  }
+
+  private initForm() {
+    this.userForm = this.formBuilder.group({
+      username: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(20)]],
+      password: ['', [Validators.required, Validators.min(8)]]
+    });
+  }
 }
--- src/app/components/page_/auth_/register/register.component.ts
+++ src/app/components/page_/auth_/register/register.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, ValidationErrors, Validators} from "@angular/forms";
+import {AuthService} from "../../../../services/auth/auth.service";
+import {Router} from "@angular/router";
+import {environment} from "../../../../../environments/environment";
+import {Title} from "@angular/platform-browser";
+import {firstValueFrom} from "rxjs";
+
 
 @Component({
   selector: 'app-register',
@@ -6,10 +13,67 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
+  submitted: boolean;
+  error: boolean;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder,
+              private _authService: AuthService,
+              private router: Router,
+              private _titleService: Title) {
+    this._titleService.setTitle("Inscription - " + environment.name);
+    this.error = false;
+    this.submitted = false;
   }
 
+  get username() {
+    return this.registerForm.get('username');
+  }
+
+  get password() {
+    return this.registerForm.get('password');
+  }
+
+  get confirmPassword() {
+    return this.registerForm.get('confirmPassword');
+  }
+
+  get mail() {
+    return this.registerForm.get('mail');
+  }
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  onSubmitForm() {
+    this.submitted = true;
+    const formValue = this.registerForm.value;
+    firstValueFrom(this._authService.register(formValue.mail, formValue.username, formValue.password))
+      .catch(error => {
+        this.submitted = false;
+        if (error.status === 401) {
+          this.error = true;
+        }
+      })
+      .finally(() => {
+        this.router.navigate(['/']).then();
+      });
+  }
+
+  samePasswordValidator(group: FormGroup): ValidationErrors | null {
+    const password = group.get('password');
+    const confirmPassword = group.get('confirmPassword');
+    return password.value === confirmPassword.value ? null : {notSame: true};
+  }
+
+  private initForm() {
+    this.registerForm = this.formBuilder.group({
+      username: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(20)]],
+      password: ['', [Validators.required, Validators.pattern("^(?=.+[0-9])(?=.+[a-z])(?=.+[A-Z])(?=.+[*.!@$%^&(){}[_\\]:;<>,.?/~_+\\-=|]).{8,32}$")]],
+      confirmPassword: ['', [Validators.required]],
+      mail: ['', [Validators.required, Validators.email]]
+    }, {validators: this.samePasswordValidator});
+  }
 }
+
