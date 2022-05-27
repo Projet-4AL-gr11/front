1aa2b22eb0ff7ca5b061c9d403f1e31fafb9007b
tbriand <briandthibaud99@gmail.com>
2022-05-27 13:58:26 +0200

    [add] page timeline

---
+++ src/app/components/page_/timeline/timeline.module.ts
@@ -0,0 +1,20 @@
+// import {VirtualScrollerModule} from "@iharbeck/ngx-virtual-scroller";
+// import {NgModule} from "@angular/core";
+// import {CommonModule} from "@angular/common";
+// import {CodeRoutingModule} from "../code/code-routing.module";
+// import {TimelineComponent} from "./timeline.component";
+//
+// @NgModule({
+//   declarations: [
+//     TimelineComponent,
+//   ],
+//   imports: [
+//     CommonModule,
+//     CodeRoutingModule,
+//     VirtualScrollerModule,
+//   ],
+//   exports: [
+//     TimelineComponent,
+//   ]
+// })
+// export class TimelineModule { }
