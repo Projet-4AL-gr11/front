2d9bba0cbd84e19fa25cd9be7bcb5a34ac675544
tbriand <briandthibaud99@gmail.com>
2022-06-15 18:19:44 +0200

    [add] dialogue-update-group

---
+++ src/app/components/dialog_/dialog-update-group/dialog-update-group.module.ts
@@ -0,0 +1,34 @@
+import {NgModule} from "@angular/core";
+import {DialogCreatePostComponent} from "../dialog-create-post/dialog-create-post.component";
+import {CommonModule} from "@angular/common";
+import {RouterModule} from "@angular/router";
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import {MatMenuModule} from "@angular/material/menu";
+import {PipesModules} from "../../../pipes/pipes.modules";
+import {PostShareCardModule} from "../../card_/post-share-card/post-share-card.module";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {FormsModule} from "@angular/forms";
+import {MatButtonModule} from "@angular/material/button";
+import {MatInputModule} from "@angular/material/input";
+
+@NgModule({
+  declarations: [
+    DialogCreatePostComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    FontAwesomeModule,
+    MatMenuModule,
+    PipesModules,
+    PostShareCardModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatButtonModule,
+    MatInputModule,
+  ],
+  exports: [
+    DialogCreatePostComponent,
+  ]
+})
+export class DialogCreatePostModule { }
