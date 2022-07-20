34da44e68602997a95ebbe9dcac991c2a6225357
tbriand <briandthibaud99@gmail.com>
2022-07-20 15:49:21 +0200

    [fix] fix dto

--- src/app/services/models/report.model.ts
+++ src/app/services/models/report.model.ts
@@ -1,19 +1,14 @@
 import {User} from "./user.model";
-import {Group} from "./group.model";
-import {Post} from "./post.model";
-import {Event} from "./event.model";
-import {Exercise} from "./exercise.model";
-import {Comment} from "./comment.model";
 
 export class Report {
   id: string;
   text: string;
   userReporter: User;
-  reportedUser?: User;
-  reportedGroup?: Group;
-  reportedPost?: Post;
-  reportedEvent?: Event;
-  reportedExercise?: Exercise;
-  reportedComment?: Comment;
+  reportedUser?: string;
+  reportedGroup?: string;
+  reportedPost?: string;
+  reportedEvent?: string;
+  reportedExercise?: string;
+  reportedComment?: string;
   nbReport?: number;
 }
