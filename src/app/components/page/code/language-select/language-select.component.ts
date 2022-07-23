865aca5f474ac1a9adcc0c3e4b13c1e4e7c080fd
ahmedine <krapulxx@gmail.com>
2022-07-24 00:01:17 +0200

    [ADD] linked event creation

--- src/app/components/page/code/language-select/language-select.component.ts
+++ src/app/components/page/code/language-select/language-select.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Router} from "@angular/router";
 import {Event} from "../../../../services/models/event.model";
 import {EventService} from "../../../../services/event/event.service";
+import {DialogCreatePostComponent} from "../../../dialog/dialog-create-post/dialog-create-post.component";
+import {MatDialog} from "@angular/material/dialog";
+import {DialogCreateEventComponent} from "../../../dialog/dialog-create-event/dialog-create-event.component";
 
 @Component({
   selector: 'app-language-select',
@@ -9,7 +12,7 @@ import {EventService} from "../../../../services/event/event.service";
   styleUrls: ['./language-select.component.css']
 })
 export class LanguageSelectComponent implements OnInit {
-
+  @Output() eventSend: EventEmitter<any> = new EventEmitter<any>();
   languages = ['Python', 'JavaScript'];
   events: Event[];
   timeLeft = Date.now();
@@ -17,7 +20,8 @@ export class LanguageSelectComponent implements OnInit {
   constructor(
     public router: Router,
     public eventService: EventService,
-  ) { }
+    public dialogReport: MatDialog,
+) { }
 
   ngOnInit(): void {
     this.eventService.getAllEvent().subscribe( res => {
@@ -27,5 +31,12 @@ export class LanguageSelectComponent implements OnInit {
 
   goToSendBox() {
     this.router.navigateByUrl("/code/sandbox");
+  }
+
+  openPopup() {
+    const event = this.dialogReport.open(DialogCreateEventComponent, {minWidth: "500px", minHeight: "121px", data: {}});
+    event.afterClosed().subscribe(() => {
+      this.eventSend.emit()
+    })
   }
 }
