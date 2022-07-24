87771032846233cca231ea00aec5a5e71c226063
ahmedine <krapulxx@gmail.com>
2022-07-24 20:03:21 +0200

    [FIX]

--- src/app/components/page/event-view/event-view.component.ts
+++ src/app/components/page/event-view/event-view.component.ts
@@ -10,6 +10,8 @@ import {ExecutionService} from "../../../services/execution/execution.service";
 import {Exercise} from "../../../services/models/exercise.model";
 import {Leaderboard} from "../../../services/models/leaderboard.model";
 import {Chronometer} from "ngx-chronometer";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {AuthService} from "../../../services/auth/auth.service";
 
 @Component({
   selector: 'app-page-event',
@@ -20,7 +22,7 @@ export class EventViewComponent implements OnInit {
 
   event: Event;
   leaderboards: Leaderboard[] = [];
-  timerState: boolean = true;
+  timerState: boolean = false;
   currentExercise: Exercise;
   chronometer: Chronometer = new Chronometer();
 
@@ -29,6 +31,8 @@ export class EventViewComponent implements OnInit {
               private _exerciseService: ExerciseService,
               private _titleService: Title,
               private _executionService: ExecutionService,
+              private _snackBar: MatSnackBar,
+              private _authService: AuthService,
   ) {
   }
 
@@ -40,8 +44,6 @@ export class EventViewComponent implements OnInit {
     });
   }
 
-
-
   async updateEvent(id: string): Promise<void> {
     this.event = await firstValueFrom(this._eventService.getEventById(id));
     this.event.exercises = [];
@@ -49,8 +51,12 @@ export class EventViewComponent implements OnInit {
     await firstValueFrom(this._executionService.getEventRanking(id)).then(eventRanking => {
       this.event.eventRanking = eventRanking;
     })
-    this.setExercise(this.event?.exercises[0]);
   }
+
+  startTimer() {
+    this.timerState = false;
+  }
+
 
   setExercise(exercise?: Exercise) {
     if (exercise != undefined) {
@@ -58,9 +64,65 @@ export class EventViewComponent implements OnInit {
     }
   }
 
-  startTimer() {
-    this.chronometer.start();
-    this.timerState = false;
+  startEvent() {
+    console.log(this.event)
+    if (!this.event?.exercises) {
+      this._snackBar.open('Une erreur c\'est produite lors de la récupération des exercises, réessayer plus tard', 'Fermer', {
+        duration: 3000
+      });
+      return;
+    } else {
+      this.timerState = true;
+      this.chronometer.start();
+      this._eventService.addParticipant(this.event.id, this._authService.getCurrentUserId()).subscribe({
+        next: () => {
+          this.startExercise(this.event.exercises[0])
+          console.log("CURRENT EXO")
+          console.log(this.currentExercise)
+        },
+        error: err => {
+          if (!environment.production) {
+            console.log(err)
+          }
+          this._snackBar.open('Il semble que vous avez déjà participer a l\'évènment', 'Fermer', {
+            duration: 3000
+          });
+        }
+      });
+    }
   }
 
+  startExercise(exercise: Exercise) {
+    this.currentExercise = exercise;
+  }
+
+  nextExercise() {
+    if ((this.event.exercises.length -1 == this.event.exercises.indexOf(this.currentExercise))) {
+      this.timerState = false;
+      this._snackBar.open('Event terminé, regarder où vous êtes dans le classement :)', 'Fermer', {
+        duration: 3000
+      });
+      return;
+    } else {
+      this.currentExercise = this.event.exercises[this.event.exercises.indexOf(this.currentExercise) + 1];
+      this.chronometer.restart()
+    }
+  }
+
+
 }
+
+
+/*
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.exercise) {
+      firstValueFrom(this._exerciseService.getExerciseTemplateWithExerciseId(this.exercise.id)).then(exerciseTemplate => {
+        this.exerciseTemplate = exerciseTemplate
+      });
+      firstValueFrom(this._executionService.getLeaderboardWithExerciseId(this.exercise.id)).then(leaderboards => {
+        this.leaderboards = leaderboards;
+      })
+    }
+  }
+
+ */
