5a1f4881e3073c934bbaa3ac85cc2db5f66c2b19
ahmedine <krapulxx@gmail.com>
2022-07-27 00:30:02 +0200

    [deploy]

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  name: "2FastHand",
+  domain: "https://2fasthand.azurewebsites.net",
+  apiBaseUrl: "https://2fasthandapi.azurewebsites.net",
 };
